9674422386252c369bf14c14d249e2f058374aff
DementedDarsh <darsh.chong@agilelab.sg>
2022-02-26 13:04:06 +0800

    Skill selection primitive done

--- client/src/Components/MainScreens/SkillSelect.jsx
+++ client/src/Components/MainScreens/SkillSelect.jsx
@@ -1,18 +1,74 @@
-import { useState } from "react"
+import { useState } from "react";
 const skills = require("../../LocalDatabase/skills");
 
 const SkillSelect = (props) => {
-const [skillList, setSkillList] = useState(skills);
+  const [skillList, setSkillList] = useState(skills);
+  const [chosenSkills, setChosenSkills] = useState([]);
 
-// const skillChoices = skillList.map((item, index) => {return (<div key={index}>item[index].name</div>)})
-const test = Object.keys(skillList).map((item, index) => {
-    return (<div key={skillList[item].name}>{skillList[item].name}</div>)
+  const handleListClick = (e) => {
+    console.log(e);
+    setChosenSkills((prevState) => [...prevState, e]);
+    const array = skillList;
+    array.splice(array.indexOf(e), 1);
+    console.log(array);
+    setSkillList(array);
+  };
+
+  const handleChosenClick = (e) => {
+    console.log(e);
+    setSkillList((prevState) => [...prevState, e]);
+    const array = chosenSkills;
+    array.splice(array.indexOf(e), 1);
+    console.log(array);
+    setChosenSkills(array);
+  };
+
+  //   const skillChoices = skillList.map((item, index) => {
+  //     return (
+  //       <div onClick={() => handleListClick(item)} key={index}>
+  //         {item.name}
+  //       </div>
+  //     );
+  //   });
+
+  const skillChoices = skillList.map((item, index) => {
+    return (
+      <tr key={index} onClick={() => handleListClick(item)}>
+        <td style={{ height: "100px" }}>
+          <img src={item.imagePath} style={{ height: "100px" }} /><br />
+          {item.name}
+        </td>
+        <td>{item.tooltipText}</td>
+      </tr>
+    );
+  });
+
+  const skillsChosen = chosenSkills.map((item, index) => {
+    return (
+      <tr key={index} onClick={() => handleChosenClick(item)}>
+        <td style={{ height: "100px", textAlign: "center", verticalAlign: "center"}}>
+          <img src={item.imagePath} style={{ height: "100px" }} /><br />
+          {item.name}
+        </td>
+        <td>{item.tooltipText}</td>
+      </tr>
+    );
   });
 
   return (
-    // <div><span></span><span>{skillChoices}</span></div>
-    <div>{test}</div>
-  )
-}
+    <div className="skillsContainer">
+      <span>
+        <table style={{width: "500px"}}>
+          <tbody>{skillsChosen}</tbody>
+        </table>
+      </span>
+      <span>
+        <table style={{width: "500px"}}>
+          <tbody>{skillChoices}</tbody>
+        </table>
+      </span>
+    </div>
+  );
+};
 
-export default SkillSelect
+export default SkillSelect;
--- client/src/LocalDatabase/skills.js
+++ client/src/LocalDatabase/skills.js
@@ -10,56 +10,54 @@ const test = async (setTestValue) => {
   for (let i = 0; i < 5; i++) {
     await interval();
     setTestValue((prevState) => prevState - 50);
-    console.log("Hi")
+    console.log("Hi");
   }
 };
 
 const hits = async (skill, weaponStats, states) => {
-    for (let i = 0; i < weaponStats; i++) {
-        states(true)
-        skill(weaponStats, states);
-        await interval();
-    }
-}
+  for (let i = 0; i < weaponStats; i++) {
+    states(true);
+    skill(weaponStats, states);
+    await interval();
+  }
+};
 
-
-module.exports = {
-  imbueFire: {
+module.exports = [
+  {
     name: "Imbue: Fire",
     tooltipText: "Imbues with fire",
-    imagePath: "test",
+    imagePath: "https://res.cloudinary.com/djtovzgnc/image/upload/v1645850009/project4/ip1rj8igc0aunx3jnn8d.png",
     effect: (weaponStats, states) => {
       console.log("Imbue Fire does additional damage for each hit");
     },
   },
-  aimedStrike: {
+  {
     name: "Aimed Strike",
     tooltipText: "An aimed attacked that never misses",
-    imagePath: "test",
+    imagePath: "https://res.cloudinary.com/djtovzgnc/image/upload/v1645850009/project4/ip1rj8igc0aunx3jnn8d.png",
     effect: async (weaponStats, states) => {
-        const skill = (weaponStats, states) => console.log(weaponStats);
-        await hits(skill, weaponStats, states);
-        states(false)
-        
+      const skill = (weaponStats, states) => console.log(weaponStats);
+      await hits(skill, weaponStats, states);
+      states(false);
     },
   },
-  rendArmor: {
+  {
     name: "Rend Armor",
     tooltipText: "Destroys the target's defenses",
-    imagePath: "test",
+    imagePath: "https://res.cloudinary.com/djtovzgnc/image/upload/v1645850009/project4/ip1rj8igc0aunx3jnn8d.png",
     effect: (weaponStats, states) => {
       console.log("Destroys the target's defenses");
     },
   },
-  vampirism: {
+  {
     name: "Vampirism",
     tooltipText:
       "You thirst for blood, on the next attack, you heal life equals to the damage dealt",
-    imagePath: "test",
+    imagePath: "https://res.cloudinary.com/djtovzgnc/image/upload/v1645850009/project4/ip1rj8igc0aunx3jnn8d.png",
     effect: (setLifeSteal, setTestValue) => {
       setLifeSteal(true);
       //   this.enemyHit(Math.floor(Math.random() * 5));
       test(setTestValue);
     },
   },
-};
+];
